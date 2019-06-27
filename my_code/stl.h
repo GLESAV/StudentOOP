0fa820482963e43b4576efc107df97a1393f389b
Greg <gregory.leonardo.savage@gmail.com>
2019-06-27 13:10:34 -0400

    new assignment done

--- my_code/stl.h
+++ my_code/stl.h
@@ -12,7 +12,7 @@ template <typename FILL>
 void print_vec(string name, const vector<FILL>& vector_of_fill)
 {
 	cout << name << endl;
-	for (FILL fill: vector_of_fill)
+	for (const FILL& fill: vector_of_fill)
 	{
 		cout << fill << "\t";
 	}
@@ -23,7 +23,7 @@ template <typename FILL>
 void print_list(string name, const list<FILL>& list_of_fill)
 {
 	cout << name << endl;
-	for (FILL fill: list_of_fill)
+	for (const FILL& fill: list_of_fill)
 	{
 		cout << fill << "\t";
 	}
@@ -35,14 +35,14 @@ template <typename CLASS>
 void print(string name, const CLASS& a_class)
 {
 	cout << name << endl;
-	for (auto fill: a_class)
+	for (const auto& fill: a_class)
 	{
 		cout << fill << "\t";
 	}
 	cout<< endl;
 	
 	
-	//this doesn't work either
+	//this doesn't work....
 	// for (CLASS::const_iterator iter=a_class.begin(); iter!=a_class.end();++iter)
 	// {
 		// cout << *iter << "\t";
